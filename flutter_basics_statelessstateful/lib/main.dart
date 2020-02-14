e31f8d9eae87cd8607026daddcc7a6813e57b7f4
tadasss <38183310+tadasss@users.noreply.github.com>
2020-02-13 19:10:30 -0600

    For stateless and stateful video

---
+++ flutter_basics_statelessstateful/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basics_statelessstateful/screens/home/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
