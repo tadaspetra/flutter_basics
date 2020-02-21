bab42ad0dea63f25309866408c2f309de3bc6add
tadasss <38183310+tadasss@users.noreply.github.com>
2020-02-20 18:57:37 -0600

    BlocListener and BlocConsumer video

---
+++ flutter_morebloc/lib/screens/home/bloc/counter_bloc.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'counter_event.dart';
+part 'counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  @override
+  CounterState get initialState => CounterState(counter: 5);
+
+  @override
+  Stream<CounterState> mapEventToState(
+    CounterEvent event,
+  ) async* {
+    if (event is IncrementEvent) {
+      yield CounterState(counter: state.counter + 1);
+    } else if (event is DecrementEvent) {
+      yield CounterState(counter: state.counter - 1);
+    }
+  }
+}
