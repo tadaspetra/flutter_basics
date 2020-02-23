ee8684b29f8f5a7e60c2aea5b2b46c024af30a4d
tadasss <38183310+tadasss@users.noreply.github.com>
2020-02-23 13:25:47 -0600

    new contents for video

---
+++ flutter_multibloc/lib/blocs/random/random_event.dart
@@ -0,0 +1,11 @@
+part of 'random_bloc.dart';
+
+abstract class RandomEvent extends Equatable {
+  const RandomEvent();
+}
+
+class GenerateRandom extends RandomEvent {
+  @override
+  // TODO: implement props
+  List<Object> get props => throw UnimplementedError();
+}
