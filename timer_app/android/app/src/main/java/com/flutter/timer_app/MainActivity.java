bf9fdf082595581a26a6fd75df02853cf3233e7e
Amateur Coder <connectwithamateur@gmail.com>
2020-04-16 19:29:35 -0500

    simple timer app

---
+++ timer_app/android/app/src/main/java/com/flutter/timer_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.flutter.timer_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ timer_app/lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 10;
+  Timer _timer;
+
+  void _startTimer() {
+    _counter = 10;
+    if (_timer != null) {
+      _timer.cancel();
+    }
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if (_counter > 0) {
+          _counter--;
+        } else {
+          _timer.cancel();
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Timer App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            (_counter > 0)
+                ? Text("")
+                : Text(
+                    "DONE!",
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 48,
+                    ),
+                  ),
+            Text(
+              '$_counter',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 48,
+              ),
+            ),
+            RaisedButton(
+              onPressed: () => _startTimer(),
+              child: Text("Start 10 second count down"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
