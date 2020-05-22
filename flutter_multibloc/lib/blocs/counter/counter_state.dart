41d8e1235b001e0d12d66d303a3a79805065b3ac
Amateur Coder <connectwithamateur@gmail.com>
2020-05-22 16:31:45 -0500

    renamed everything to exact video name

--- flutter_multibloc/lib/blocs/counter/counter_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'counter_bloc.dart';
-
-class CounterState extends Equatable {
-  final int counter;
-  const CounterState({this.counter});
-  @override
-  List<Object> get props => [counter];
-}
