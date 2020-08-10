1cf5c2375b1548ea17e2c26415c4e28bfac8287f
Amateur Coder <connectwithamateur@gmail.com>
2020-08-10 18:21:43 -0500

    Simple Unit Tests

---
+++ 017_unit_testing/lib/screens/home.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:unit_testing/controllers/list_controller.dart';
+import 'package:unit_testing/models/todo.dart';
+import 'package:unit_testing/services/database.dart';
+import 'package:unit_testing/widgets/todo_card.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final TextEditingController _todoController = TextEditingController();
+  ListController listController = ListController(database: Database());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List App"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Add Todo Here:",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.all(20),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _todoController,
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      if (_todoController.text != "") {
+                        listController
+                            .addTodo(TodoModel(_todoController.text, false));
+                        //Database()
+                        //.addTodo(_todoController.text, controller.user.uid);
+                        _todoController.clear();
+                        setState(() {});
+                      }
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          RaisedButton(
+            child: Text("Load Todo from Database"),
+            onPressed: () {
+              listController.loadFromDatabase();
+              setState(() {});
+            },
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Your Todos",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: listController.todoList.length,
+              itemBuilder: (_, index) {
+                return TodoCard(index: index, listController: listController);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ 017_unit_testing/lib/services/database.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:unit_testing/models/todo.dart';
+
+class Database {
+  Future<TodoModel> loadDatabase() async {
+    try {
+      Future.delayed(Duration(seconds: 3));
+      print("this got called?!?");
+      return TodoModel("From Database", true);
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
+}
