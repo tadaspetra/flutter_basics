76b2ace5dea0743377d5b9f73ba608ad4a1becec
Tadas Petra <connectwithamateur@gmail.com>
2020-09-08 17:21:13 -0500

    Widget testing working

---
+++ 018_widget_testing/lib/services/database.dart
@@ -0,0 +1,13 @@
+import 'package:unit_testing/models/todo.dart';
+
+class Database {
+  Future<TodoModel> loadDatabase() async {
+    try {
+      await Future.delayed(Duration(seconds: 3));
+      return TodoModel("From Database", true);
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
+}
