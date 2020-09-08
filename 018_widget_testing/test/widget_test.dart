8677c49446a24df38b7dca9535c6bb22e262deb0
Tadas Petra <connectwithamateur@gmail.com>
2020-09-08 18:04:41 -0500

    Updated from recording

--- 018_widget_testing/test/widget_test.dart
+++ 018_widget_testing/test/widget_test.dart
@@ -3,45 +3,31 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_testing/screens/home.dart';
 
 void main() {
-  // Define a test. The TestWidgets function also provides a WidgetTester
-  // to work with. The WidgetTester allows building and interacting
-  // with widgets in the test environment.
-  testWidgets('Add a todo', (WidgetTester tester) async {
-    // Create the widget by telling the tester to build it.
-    await tester.pumpWidget(MaterialApp(home: Home()));
-
-    // Create the Finders.
+  testWidgets("add a todo", (WidgetTester tester) async {
+    //find all widgets needed
     final addField = find.byKey(ValueKey("addField"));
     final addButton = find.byKey(ValueKey("addButton"));
 
-    final messageFinder = find.text('Get Groceries');
-
-    await tester.enterText(addField, 'Get Groceries');
-
+    //execute the actual test
+    await tester.pumpWidget(MaterialApp(home: Home()));
+    await tester.enterText(addField, "Make Widget Testing Video");
     await tester.tap(addButton);
+    await tester.pump(); //rebuilds your widget
 
-    await tester.pump(); //rebuilds
-
-    // Use the `findsOneWidget` matcher provided by flutter_test to
-    // verify that the Text widgets appear exactly once in the widget tree.
-    expect(messageFinder, findsOneWidget);
+    //check outputs
+    expect(find.text("Make Widget Testing Video"), findsOneWidget);
   });
 
-  testWidgets('Add a todo from database', (WidgetTester tester) async {
-    // Create the widget by telling the tester to build it.
-    await tester.pumpWidget(MaterialApp(home: Home()));
-
-    // Create the Finders.
+  testWidgets("add from database", (WidgetTester tester) async {
+    //find all widgets needed
     final loadFromDatabase = find.byKey(ValueKey("loadFromDatabase"));
 
-    final messageFinder = find.text('From Database');
-
+    //execute the actual test
+    await tester.pumpWidget(MaterialApp(home: Home()));
     await tester.tap(loadFromDatabase);
+    await tester.pump(Duration(seconds: 2)); //rebuilds your widget
 
-    await tester.pump(Duration(seconds: 4)); //rebuilds
-
-    // Use the `findsOneWidget` matcher provided by flutter_test to
-    // verify that the Text widgets appear exactly once in the widget tree.
-    expect(messageFinder, findsOneWidget);
+    //check outputs
+    expect(find.text("From Database"), findsOneWidget);
   });
 }
