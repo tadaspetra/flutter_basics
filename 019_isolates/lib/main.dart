aa60db1dce60992967257a73a81acb6b8012c316
Tadas Petra <connectwithamateur@gmail.com>
2020-10-28 19:23:39 -0500

    simplified

--- 019_isolates/lib/main.dart
+++ 019_isolates/lib/main.dart
@@ -12,15 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -70,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Isolates Demo"),
       ),
       body: Center(
         child: Column(
