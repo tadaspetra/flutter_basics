3ec154f9227d83285d864280bc417d8fbdc7f9fb
Tadas Petra <connectwithamateur@gmail.com>
2021-02-10 10:02:21 -0600

    debugging

---
+++ 022_debugging/lib/screens/home/home.dart
@@ -0,0 +1,61 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateless vs. Stateful"),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          FirstColumn(),
+          SecondColumn(),
+        ],
+      ),
+    );
+  }
+}
+
+class FirstColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[Text("Like This: ")],
+    );
+  }
+}
+
+class SecondColumn extends StatefulWidget {
+  @override
+  _SecondColumnState createState() => _SecondColumnState();
+}
+
+class _SecondColumnState extends State<SecondColumn> {
+  bool liked = false;
+
+  void updateLike() {
+    potentialException();
+    setState(() {
+      liked = !liked;
+    });
+  }
+
+  void potentialException() {
+    if (Random().nextBool()) {
+      throw Exception();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[IconButton(icon: Icon(liked ? Icons.star : Icons.star_border), onPressed: updateLike)],
+    );
+  }
+}
